ed4d23fdebe7f3b4753c229ff48d1aface134c86
Nathan Peterson <nalexpeterson@gmail.com>
2016-12-07 21:04:50 -0800

    Initial commit

---
+++ src/app/parallax/parallax.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { ng2Parallax  } from '../../../node_modules/ang2-parallax/ng2parallax';
+
+@Component({
+  selector: 'my-parallax',
+  templateUrl: './parallax.component.html',
+  styleUrls: ['./parallax.component.css'],
+})
+export class ParallaxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
