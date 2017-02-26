882904ac6af28e682f03d4664719e8261520a045
Nathan Peterson <nalexpeterson@gmail.com>
2017-02-26 14:56:31 -0800

    Linked together fb auth and database user info.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
@@ -45,6 +45,7 @@ const authConfig = {
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(firebaseConfig, authConfig),
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params }   from '@angular/router';
 import { Location }                 from '@angular/common';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -9,21 +10,47 @@ import { AngularFire, FirebaseListObservable } from 'angularfire2';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-   public model;
-   public authState;
+   model;
+   authState;
+   form;
+   currentUser;
+   item: FirebaseListObservable<any>;
 
   constructor(public af: AngularFire) {
     this.model = { email: "", password: "" };
+
+    this.item = af.database.list('/users/');
+
     this.af.auth.subscribe((auth) => {
      this.authState = auth;
     });
   }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl(''),
+      password: new FormControl(''),
+      name: new FormControl('')
+    });
   }
 
-  public submit() {
-    this.af.auth.createUser(this.model).then(() =>  alert);
+  public submit(data) {
+    this. model = { email: data.email, password: data.password }
+
+    this.af.auth.createUser(this.model).then((success) =>{
+        console.log(data);
+        this.af.auth.subscribe((auth) => {
+          this.currentUser = auth.uid;
+        })
+    }).then(() =>
+      this.item.push({
+        uid: this.currentUser,
+        name: data.name,
+        email: data.email
+      })
+    ).catch((err) => {
+        alert(err);
+    });
   }
 
 }
