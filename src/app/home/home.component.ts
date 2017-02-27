523e25aabbee37d12ea6c0dd290e87d7fe2fef57
Nathan Peterson <nalexpeterson@gmail.com>
2017-02-27 01:19:50 -0800

    Minor changes to tweak jquery | fails on live site

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,21 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 import { ActivatedRoute, Params }   from '@angular/router';
 import { Location }                 from '@angular/common';
-
+declare var $: any;
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent{
 
-  constructor() {
+  constructor(private _elRef: ElementRef) {
     // private route: ActivatedRoute,
     // private location: Location,
   }
 
-  ngOnInit() {
+  ngAfterViewInit(){
+    $(document).ready(function(){
+      $('.my-countdown').countdown({
+        until:$.countdown.UTCDate(
+          -5,2017,2,28,12,0,0
+        )
+      });
+    });
   }
 
 }
