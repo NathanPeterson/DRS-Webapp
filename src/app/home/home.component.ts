f04c8776c4d49839d6ff0fad8c7f178c5e6829b0
Nathan Peterson <nalexpeterson@gmail.com>
2017-04-06 15:27:48 -0400

    Add become reviewer button on home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef } from '@angular/core';
 import { ActivatedRoute, Params }   from '@angular/router';
 import { Location }                 from '@angular/common';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 declare var $: any;
 
 @Component({
@@ -9,17 +10,19 @@ declare var $: any;
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent{
+  public authState;
 
-  constructor(private _elRef: ElementRef) {
-    // private route: ActivatedRoute,
-    // private location: Location,
+  constructor(private _elRef: ElementRef, public af: AngularFire) {
+    this.af.auth.subscribe((auth) => {
+      this.authState = auth;
+    });
   }
 
   ngAfterViewInit(){
     $(document).ready(function(){
       $('.my-countdown').countdown({
         until:$.countdown.UTCDate(
-          -5,2017,2,28,18,0,0
+          -5,2017,4,28,18,0,0
         )
       });
     });
