8fb84f71419886f4fb8ec741086749cb8236109d
Nathan Peterson <nalexpeterson@gmail.com>
2017-04-10 03:56:42 -0400

    Working on proposal app, grabbing data from db

--- src/app/proposal-app/proposal-app.component.ts
+++ src/app/proposal-app/proposal-app.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { FileItem } from '../directives/file-item';
 import { UploadFilesService } from '../services/upload-files.service';
+import { Validators, FormBuilder } from '@angular/forms';
+import { InstitutionService } from '../services/institution.service';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-proposal-app',
@@ -12,8 +15,48 @@ export class ProposalAppComponent {
   isDropZoneOver:boolean = false;
   isEnabledUpload: boolean = true;
   files: Array<FileItem[]> = [];
+  form;
+  institutions;
+  currentUser;
 
-  constructor(public uploadFilesService: UploadFilesService) {
+  constructor(private uploadFilesService: UploadFilesService,
+              private formBuilder: FormBuilder,
+              private institutionService: InstitutionService,
+              private af: AngularFire) {
+
+      this.institutions = this.institutionService.get();
+      this.af.auth.subscribe((auth) => {
+       this.currentUser = auth.uid;
+      });
+      const currentUserInfo = this.af.database.list("/users", {
+        preserveSnapshot: true,
+        query: {
+          equalTo: this.currentUser
+        }
+      }).subscribe(snapshots=>{
+        snapshots.forEach(snapshot => {
+            console.log(snapshot.lastName, snapshot.lastName);
+        });
+      })
+      // currentUserInfo.subscribe(
+      //   val => console.log(val)
+      // )
+      console.log("This is my current user" + currentUserInfo)
+  }
+
+  ngOnInit() {
+
+
+    this.form = this.formBuilder.group({
+      fname: this.formBuilder.control(''),
+      mi: this.formBuilder.control(''),
+      lname: this.formBuilder.control(''),
+
+      univ: this.formBuilder.control(''),
+      department: this.formBuilder.control(''),
+      discipline: this.formBuilder.control(''),
+      subdiscipline: this.formBuilder.control(''),
+    });
   }
 
   public fileOverDropZone(e:any):void {
