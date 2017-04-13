b50e0ad87dd7418d23bd00471c043ec0668e42bf
Nathan Peterson <nalexpeterson@gmail.com>
2017-04-13 17:20:29 -0400

    Redirect users that login successfully to home page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { ActivatedRoute, Params, Router }   from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +11,8 @@ export class LoginComponent implements OnInit {
   public model;
   public authState;
 
-  constructor(public af: AngularFire) {
+  constructor(public af: AngularFire,
+              private router: Router) {
     this.model = { email: "", password: "" };
       this.af.auth.subscribe((auth) => {
         this.authState = auth;
@@ -20,7 +22,7 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
   }
   public submit() {
-    this.af.auth.login(this.model).catch(alert);
+    this.af.auth.login(this.model).then((success) => this.router.navigate(['/home'])).catch(alert)
   }
 
   public logout() {
