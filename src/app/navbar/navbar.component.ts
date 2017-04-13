7ae0d602268ed56244fb6a96f230d550d90297a1
Nathan Peterson <nalexpeterson@gmail.com>
2017-04-13 19:23:29 -0400

    Create dynamic navbar, add tabs based on account type logged in

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -8,17 +8,33 @@ import { AngularFire, FirebaseListObservable } from 'angularfire2';
 })
 export class NavbarComponent implements OnInit {
   public authState;
+  private reviewer =false;
+  private admin =false;
 
   constructor(public af: AngularFire) {
     this.af.auth.subscribe((auth) => {
       this.authState = auth;
+      if(this.authState){
+        this.currentUser = this.authState.uid;
+        const currentUserInfo = this.af.database.object('/users/' + this.currentUser);
+        currentUserInfo.subscribe((info) =>{
+          if(info.accountType === "reviewer"){
+            this.reviewer = true;
+          }else if(info.accountType === "admin"){
+            this.admin = true;
+          }
+        });
+      }
     });
   }
 
   ngOnInit() {
   }
 
+
   public logout() {
+    this.reviewer =false;
+    this.admin = false;
     this.af.auth.logout();
   }
 
