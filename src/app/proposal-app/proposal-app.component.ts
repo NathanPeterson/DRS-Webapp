a1fa44bca8b095ffe11a127b6f650d40678e6b66
Nathan Peterson <nalexpeterson@gmail.com>
2017-04-13 16:38:35 -0400

    Restrict users that are not logged in to login page when trying to submit porposal

--- src/app/proposal-app/proposal-app.component.ts
+++ src/app/proposal-app/proposal-app.component.ts
@@ -5,6 +5,7 @@ import { Validators, FormBuilder } from '@angular/forms';
 import { InstitutionService } from '../services/institution.service';
 import { ProposalDomainService } from '../services/proposal-domain.service';
 import { AngularFire, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2';
+import { CanActivate, Router } from '@angular/router';
 
 @Component({
   selector: 'app-proposal-app',
@@ -28,13 +29,14 @@ export class ProposalAppComponent {
               private formBuilder: FormBuilder,
               private institutionService: InstitutionService,
               private af: AngularFire,
-              private proposalDomains: ProposalDomainService) {
+              private proposalDomains: ProposalDomainService,
+              private router: Router) {
 
       this.institutions = this.institutionService.get();
       this.domains = this.proposalDomains.get();
       this.af.auth.subscribe((auth) => {
        this.authState = auth;
-      });
+     });
   }
 
   ngOnInit() {
@@ -50,24 +52,6 @@ export class ProposalAppComponent {
 
       propDomain: this.formBuilder.control(''),
       propTitle: this.formBuilder.control(''),
-    });
-
-    this.currentUser = this.authState.uid;
-    const currentUserInfo = this.af.database.object('/users/' + this.currentUser);
-    currentUserInfo.subscribe((info) =>{
-      this.form = this.formBuilder.group({
-        fname: this.formBuilder.control(info.firstName),
-        mi: this.formBuilder.control(info.mi),
-        lname: this.formBuilder.control(info.lastName),
-
-        univ: this.formBuilder.control(info.university),
-        department: this.formBuilder.control(info.department),
-        discipline: this.formBuilder.control(info.discipline),
-        subdiscipline: this.formBuilder.control(info.subdiscipline),
-
-        propDomain: this.formBuilder.control(''),
-        propTitle: this.formBuilder.control(''),
-      })
     });
   }
 
@@ -102,7 +86,30 @@ export class ProposalAppComponent {
  }
 
  proposal(){
-   this.isSubmitting = !this.isSubmitting;
+
+   if (this.authState) {
+     this.currentUser = this.authState.uid;
+     this.isSubmitting = !this.isSubmitting;
+     const currentUserInfo = this.af.database.object('/users/' + this.currentUser);
+     currentUserInfo.subscribe((info) =>{
+       this.form = this.formBuilder.group({
+         fname: this.formBuilder.control(info.firstName),
+         mi: this.formBuilder.control(info.mi),
+         lname: this.formBuilder.control(info.lastName),
+
+         univ: this.formBuilder.control(info.university),
+         department: this.formBuilder.control(info.department),
+         discipline: this.formBuilder.control(info.discipline),
+         subdiscipline: this.formBuilder.control(info.subdiscipline),
+
+         propDomain: this.formBuilder.control(''),
+         propTitle: this.formBuilder.control(''),
+       })
+     });
+     return true;
+   }
+   this.router.navigate(['/login']);
+   return false;
  }
 
 }
