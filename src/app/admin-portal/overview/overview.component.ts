e3cf07fd128aa1a0c9a7a7fe3f1682cd192fab2a
Nathan Peterson <nalexpeterson@gmail.com>
2017-04-19 02:31:58 -0400

    Create dynamic stats for admin | add email to table

--- src/app/admin-portal/overview/overview.component.ts
+++ src/app/admin-portal/overview/overview.component.ts
@@ -12,6 +12,11 @@ export class AdminOverviewComponent implements OnInit {
   currentUser;
   accountArray=[];
   totalUsers;
+  MAX_REVIEWERS;
+  totalReviewers;
+  totalAdmins;
+  totalProposals;
+  totalReviewerApps;
 
   constructor(private af: AngularFire,private router: Router) {
     this.af.auth.subscribe((auth) => {
@@ -26,17 +31,33 @@ export class AdminOverviewComponent implements OnInit {
     const listOfUsers = this.af.database.list('/users/');
     listOfUsers.subscribe((users) =>{
       this.accountArray = [];
+      this.totalReviewers =0;
+      this.MAX_REVIEWERS = 30;
+      this.totalAdmins = 0;
+      this.totalProposals = 0;
+      this.totalReviewerApps = 0;
+
       users.forEach(info =>{
         let reviewApp = false;
         let proposalApp = false;
         if(info.reviewerApplication){
           reviewApp = true;
+          this.totalReviewerApps++;
         };
         if(info.proposals){
           proposalApp = true;
+          for(let proposal in info.proposals){
+            this.totalProposals++;
+          }
+        }
+        if(this.isReviewer(info)){
+          this.totalReviewers++;
+        }else if(this.isAdmin(info)){
+          this.totalAdmins++;
         }
         this.accountArray.push({
           username: info.username,
+          email: info.email,
           uid: info.uid,
           pApp: proposalApp,
           rApp: reviewApp,
@@ -46,5 +67,12 @@ export class AdminOverviewComponent implements OnInit {
       })
       this.totalUsers = this.accountArray.length;
     });
+  }
+
+  isReviewer(data){
+    return data.accountType === 'reviewer';
+  }
+  isAdmin(data){
+    return data.accountType === 'admin';
   }
 }
