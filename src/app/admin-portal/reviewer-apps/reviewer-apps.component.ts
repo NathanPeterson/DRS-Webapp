a3b5e11fcbaeeb612b6b79e03901103a13f3e3a7
Nathan Peterson <nalexpeterson@gmail.com>
2017-04-19 04:50:55 -0400

    Start retreiving reviewer applications for admin | broke

--- src/app/admin-portal/reviewer-apps/reviewer-apps.component.ts
+++ src/app/admin-portal/reviewer-apps/reviewer-apps.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { AngularFire, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2';
+import { ProposalDomainService } from '../../services/proposal-domain.service';
 
 @Component({
   selector: 'app-admin-reviewer-apps',
@@ -10,14 +11,38 @@ import { AngularFire, FirebaseListObservable, FirebaseObjectObservable } from 'a
 
 export class AdminReviewerAppsComponent implements OnInit {
   authState;
-  
-  constructor(private af: AngularFire,private router: Router) {
+  domains;
+  reviewApplicationArray =[];
+
+  constructor(private af: AngularFire,
+              private router: Router,
+              private proposalDomains: ProposalDomainService,) {
     this.af.auth.subscribe((auth) => {
      this.authState = auth;
     });
+
+    this.domains = this.proposalDomains.get();
   }
 
   ngOnInit() {
-  }
 
+  }
+  loadData(){
+    const listOfUsers = this.af.database.list('/reviewerApplications/');
+    listOfUsers.subscribe((applications) =>{
+      this.reviewApplicationArray = [];
+      applications.forEach(info =>{
+        let reviewApp;
+        let app = this.af.database.object('/users/' + info.uid +'/reviewerApplication/');
+        app.subscribe(app =>{
+              reviewApp = app;
+              this.reviewApplicationArray.push({
+                [reviewApp.highdegree]: reviewApp.highdegree,
+                [reviewApp.likability]: reviewApp.likeability,
+                [reviewApp.pickout]: reviewApp.pickout,
+              });
+        });
+      })
+    })
+  }
 }
