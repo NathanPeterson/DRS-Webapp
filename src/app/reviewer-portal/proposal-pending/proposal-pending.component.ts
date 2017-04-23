12b8ec99a4e7ee43087d0e9fb46c2d33a35985c9
Nathan Peterson <nalexpeterson@gmail.com>
2017-04-22 22:00:10 -0400

    load in proposals from firebase

--- src/app/reviewer-portal/proposal-pending/proposal-pending.component.ts
+++ src/app/reviewer-portal/proposal-pending/proposal-pending.component.ts
@@ -9,73 +9,61 @@ import { AngularFire, FirebaseListObservable, FirebaseObjectObservable } from 'a
 })
 export class ProposalPendingComponent implements OnInit {
   authState;
-  currentUser;
-  accountArray=[];
-  currentTotal;
-  MAX_REVIEWERS;
-  totalReviewers;
-  totalAdmins;
-  totalProposals;
-  flag;
+  files;
+  proposalsArray;
 
   constructor(private af: AngularFire,private router: Router) {
     this.af.auth.subscribe((auth) => {
      this.authState = auth;
     });
-    this.flag = {
-      key: 'uid',
-      val: '',
-      triggered: false,
-    }
   }
 
   ngOnInit() {
-    this.loadData();
+    this.loadProposals();
   }
-  loadData(){
-    let listOfUsers = this.af.database.list('/users/',{
-        query: {
-          orderByChild: this.flag.key,
-        }
-      });
 
-    listOfUsers.subscribe((users) =>{
-      this.accountArray = [];
-      this.totalReviewers =0;
-      this.MAX_REVIEWERS = 30;
-      this.totalProposals = 0;
-
-      users.forEach(info =>{
-        let proposalApp = false;
-        if(info.proposals){
-          proposalApp = true;
-          for(let proposal in info.proposals){
-            this.totalProposals++;
-          }
-        }
-        if(this.isReviewer(info)){
-          this.totalReviewers++;
-        }else if(this.isAdmin(info)){
-          this.totalAdmins++;
-        }
-        this.accountArray.push({
-          username: info.username,
-          email: info.email,
-          uid: info.uid,
-          pApp: proposalApp,
-          status: info.accountType,
-          paid: false,
+  loadProposals(){
+    let listOfProposals = this.af.database.list('/proposals/');
+    listOfProposals.subscribe((proposals) => {
+      this.proposalsArray=[];
+      proposals.forEach(proposal =>{
+        let currentFiles =[];
+        let title = proposal.$key;
+        let uid = proposal.owner;
+        let proposalFiles = this.af.database.list('/proposals/' + title + '/files/');
+        proposalFiles.subscribe((files)=>{
+          let i=0;
+          files.forEach(file=>{
+            currentFiles[i] ={
+              fileName: file.fileName,
+              fileURL: file.url
+            };
+            i++;
+            console.log(currentFiles)
+          });
         });
-      })
-      this.currentTotal = this.accountArray.length;
+
+        this.proposalsArray.push({
+          title: proposal.$key,
+          uid: proposal.owner,
+          files: currentFiles,
+        });
+
+      });
+      console.log(this.proposalsArray)
     });
   }
+
 
   isReviewer(data){
     return data.accountType === 'reviewer';
   }
   isAdmin(data){
     return data.accountType === 'admin' || data.accountType === 'owner';
+  }
+
+  goToUrl(data){
+    window.location.href='data.fileURL';
   }
 
 
